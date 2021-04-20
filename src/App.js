d5013923938eeeeda27b242cbbba4fd9c4d60c3b
joonasrom <jpr97@luukku.com>
2021-04-20 14:09:22 +0300

    Fixing api requests

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 
 function QuizList() {
   const [quizzes, setQuizzes] = React.useState([]);
@@ -8,23 +8,33 @@ function QuizList() {
   }, []);
 
   const getQuizzes = () => {
-    fetch('https://ohjelmistoprojekti-1-backend.herokuapp.com/api/question/4')
+    fetch('https://ohjelmistoprojekti-1-backend.herokuapp.com/api/quiz/list')
       .then(response => response.json())
-      .then(responseData => setQuizzes(responseData.data))
+      .then(responseData => 
+          setQuizzes(responseData)
+          )
       .catch(err => console.error(err))
+
+      console.log(quizzes);
   }
+
   return (
     <div>
-      <h2> Question </h2>
+      <h2> Quizzes </h2>
       <table>
         <tbody>
           <tr>
+            <th>Quiz </th>
             <th>Question </th>
+            <th>Answer </th>
           </tr>
           {
-            quizzes.map((question) =>
-              <tr key={question.questionid}>
-                <td>{question.questionline}</td>
+            quizzes.map((quiz) =>
+              <tr key={quiz.quizId}>
+                <td>{quiz.quizName}</td>
+                
+                <td>{quiz.question.questionline}</td>
+                <td>{quiz.question.answers}</td>
               </tr>
             )
           }
