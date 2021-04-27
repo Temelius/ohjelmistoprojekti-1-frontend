14718296779e14bae3f30aa7fbb49cc4e9d00564
joonasrom <jpr97@luukku.com>
2021-04-27 14:50:05 +0300

    Work in progress, fixing radio form.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 function QuizList() {
   const [selectedQuiz, setSelectedQuiz] = React.useState([]);
   const [questions, setQuestions] = React.useState([]);
-  
+  const [ans, setAns] = React.useState("");
   useEffect(() => {
     getselectedQuiz();
   }, []);
@@ -12,32 +12,88 @@ function QuizList() {
     fetch('https://ohjelmistoprojekti-1-backend.herokuapp.com/api/quiz/2')
       .then(response => response.json())
       .then(responseData => {
-          setSelectedQuiz(responseData);
-          setQuestions(responseData.question);
+        setSelectedQuiz(responseData);
+        setQuestions(responseData.question);
       })
       .catch(err => console.error(err))
-
-      console.log(selectedQuiz);
   }
+
+
+  const sendAnswer = () => {
+    fetch('https://ohjelmistoprojekti-1-backend.herokuapp.com/api/useranswers', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        userAnswerLine: ans,
+        
+      })
+    
+    })
+
+    alert('Valitsin vastauksen ' + ans + '. Vastaus tallennettu!')
+  }
+
+  const inputChanged = (event) => {
+    setAns(event.target.value);
+  }
+
 
   return (
     <div>
-      <h2> {selectedQuiz.quizName} </h2>
-      <table>
-        <tbody>
-          <tr>
-            <th>Question </th>
-            <th>Answer </th>
-          </tr>
-          {
-            questions.map((quiz) =>
-              <tr key={quiz.questionid}>
-                <td>{quiz.questionline}</td>
+      <center>
+        <h2> {selectedQuiz.quizName} </h2>
+        <hr />
+        <form onSubmit={sendAnswer}>
+          <table>
+            <tbody>
+
+              <tr>
+                <th>Question </th>
+                <th>Answer </th>
+
               </tr>
-            )
-          }
-        </tbody>
-      </table>
+
+              {
+                questions.map((quiz) =>
+
+                  <tr key={quiz.questionid}>
+                    <td>{quiz.questionline}</td>
+
+                    <td>{quiz.answers.map((answer) =>
+
+                      <tr key={answer.answerid}>
+
+                        <td>
+
+                          <input type="radio" 
+                          value={answer.answerline} 
+                          name="useranswerline"
+                          onChange={inputChanged}
+                          />
+                          {answer.answerline}
+                        </td>
+
+
+                      </tr>
+
+                    )}
+                      <button type="submit">Vastaa</button>
+
+                    </td>
+
+                  </tr>
+
+                )
+
+              }
+
+            </tbody>
+          </table>
+        </form>
+      </center>
     </div>
   )
 
